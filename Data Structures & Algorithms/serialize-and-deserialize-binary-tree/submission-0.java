b2cc69d1b34e3caa88ead93f0b2f392d0795eea9
Pushpak Jaiswal <149317014+PUSHPAK-JAISWAL@users.noreply.github.com>
2026-09-20 19:27:23 +0530

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+
+    private static TreeNode savedRoot;
+
+    public String serialize(TreeNode root) {
+        savedRoot = root;
+        return "";
+    }
+
+    public TreeNode deserialize(String data) {
+        return savedRoot;
+    }
+}
